5118617ba00e99f0b3c82064df55729ecdeff913
Edoardo Foschi <efoschi@e3r5p2.42roma.it>
2024-12-05 12:00:54 +0100

    pierpaolo

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
+/*   By: efoschi <efoschi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/14 10:18:50 by msisto            #+#    #+#             */
-/*   Updated: 2024/12/05 10:17:10 by msisto           ###   ########.fr       */
+/*   Updated: 2024/12/05 11:57:58 by efoschi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,17 +65,20 @@
 
 /*cmd structs*/
 
-typedef struct s_cmd {
+typedef struct s_cmd
+{
 	int	type;
 }	t_cmd;
 
-typedef struct s_execcmd {
+typedef struct s_execcmd
+{
 	int		type;
 	char	*argv[MAXARGS];
 	char	*eargv[MAXARGS];
 }	t_execcmd;
 
-typedef struct s_redircmd {
+typedef struct s_redircmd
+{
 	int			type;
 	struct cmd	*cmd;
 	char		*file;
@@ -84,21 +87,24 @@ typedef struct s_redircmd {
 	int			fd;
 }	t_redircmd;
 
-typedef struct s_pipecmd {
+typedef struct s_pipecmd
+{
 	int			type;
 	struct cmd	*left;
 	struct cmd	*right;
 }	t_pipecmd;
 
-typedef struct s_listcmd {
+typedef struct s_listcmd
+{
 	int			type;
 	struct cmd	*left;
 	struct cmd	*right;
 }	t_listcmd;
 
-typedef struct s_backcmd {
-	int	type;
-	struct cmd *cmd;
+typedef struct s_backcmd
+{
+	int			type;
+	struct cmd	*cmd;
 }	t_back_cmd;
 
 /*function delle struct da fare:
