757544cb4e58acdfd442dfdaad885f887faffa3a
foschiedoardo@gmail.com <foschiedoardo@gmail.com>
2025-02-11 11:37:27 +0100

    tab

--- utils/builtin_utils.c
+++ utils/builtin_utils.c
@@ -14,19 +14,19 @@
 
 int control_bt(char *input, char **env)
 {
-    if (!input)
-        return (1);
-    if (ft_strncmp(input, "pwd", 3) == 0)
-        return (builtin_pwd(env));
-    else if (ft_strncmp(input, "cd", 2) == 0)
-        return (builtin_cd(input, env));
-    else if (ft_strncmp(input, "env", 3) == 0)
-        return (builtin_env(env, 0));
-    else if (ft_strncmp(input, "exit", 4) == 0)
-        return (builtin_exit(input));
-    else if (ft_strncmp(input, "export", 6) == 0)
-        return (builtin_export(input, env));
-    // else if (ft_strnstr(input, "unset", 6) == 0 && ecmd->argv[1])
-    //     return (builtin_unset(env, cur, ecmd->argv[1]));
-    return (1);
+	if (!input)
+		return (1);
+	if (ft_strncmp(input, "pwd", 3) == 0)
+		return (builtin_pwd(env));
+	else if (ft_strncmp(input, "cd", 2) == 0)
+		return (builtin_cd(input, env));
+	else if (ft_strncmp(input, "env", 3) == 0)
+		return (builtin_env(env, 0));
+	else if (ft_strncmp(input, "exit", 4) == 0)
+		return (builtin_exit(input));
+	else if (ft_strncmp(input, "export", 6) == 0)
+		return (builtin_export(input, env));
+	//else if (ft_strnstr(input, "unset", 6) == 0 && ecmd->argv[1])
+	//	return (builtin_unset(env, cur, ecmd->argv[1]));
+	return (1);
 }
