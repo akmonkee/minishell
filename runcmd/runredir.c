976a15313426a660dbfb1bf493ed07b4d85ca08e
akmonkee <marcosisto180704@gmail.com>
2025-02-14 11:17:43 +0100

    readline doenst leak any more in here_doc but it cant redir the output in a file

--- runcmd/runredir.c
+++ runcmd/runredir.c
@@ -6,7 +6,7 @@
 /*   By: msisto <msisto@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 13:19:27 by msisto            #+#    #+#             */
-/*   Updated: 2025/02/13 11:10:56 by msisto           ###   ########.fr       */
+/*   Updated: 2025/02/14 11:14:57 by msisto           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ int	eof_checker(char *line, char *rule)
 	i = 0;
 	if (!line)
 		return (1);
-	while (line[i] != '\0' || line[i] != '\n' || rule[i] != '\0')
+	while (line[i] != '\0' || rule[i] != '\0')
 	{
 		if (line[i] != rule[i])
 			return (0);
 		i++;
 	}
-	if ((line[i] == '\n'|| line[i] == '\0') && rule[i] == '\0')
+	if (line[i] == '\0' && rule[i] == '\0')
 		return (1);
 	return (0);
 }
@@ -46,6 +46,7 @@ void	here_doc(t_redircmd *rcmd, char *rule)
 		if (line)
 		{
 			write(fd, line, ft_strlen_g(line));
+			write(fd, "\n", 1);
 			free(line);
 		}
 	}
@@ -63,16 +64,13 @@ void	here_doc(t_redircmd *rcmd, char *rule)
 void	runredir(t_cmd *cmd, char **envp)
 {
 	t_redircmd	*rcmd;
-
 	rcmd = (t_redircmd *)cmd;
 	if (rcmd->mode == O_RDONLY)
 		close(0);
 	if (rcmd->mode > O_RDONLY && rcmd->here_doc == 0)
 		close(1);
 	if (rcmd->here_doc == 1)
-	{
 		here_doc(rcmd, rcmd->file);
-	}
 	else
 		open(rcmd->file, rcmd->mode, 0777);
 	runcmd(rcmd->cmd, envp);
